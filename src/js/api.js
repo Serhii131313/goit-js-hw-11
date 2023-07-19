a00eeba826631084946993ef4fece26857596258
Serhii131313 <serhii.tarasov.skan@gmail.com>
2023-07-19 11:02:23 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,19 +1,17 @@
 import axios from "axios";
 
 export const fetchImages = async (inputValue, pageNr) => {
-    return await fetch(
-        `https://pixabay.com/api/?key=37930046-394bc7b158c4cfc51204a831a&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
-    )
-        .then(async response => {
-            if (!response.ok) {
-                if (response.status === 404) {
-                    return [];
-                }
-                throw new Error(response.status);
-            }
-            return await response.json();
-        })
-        .catch(error => {
-            console.error(error);
-        });
+    try {
+        const response = await axios.get(
+            `https://pixabay.com/api/?key=37930046-394bc7b158c4cfc51204a831a&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+        );
+
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            return [];
+        }
+    } catch (error) {
+        console.error(error);
+    }
 };
